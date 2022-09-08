c1f1e856df3a042bac88f25ff821e12bba4c0fa4
tjalp <60233996+tjalp@users.noreply.github.com>
2022-09-08 17:33:59 +0200

    fix: escape

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 p-0">
         <AnchorCard title="Toggle Dark Mode (WIP)" href="">Switch between light and dark mode</AnchorCard>
         <AnchorCard title="Anchor Repository" href="https://github.com/tjalp/anchor/">Check out the source code of Anchor</AnchorCard>
-        <AnchorCard title="tjalp's Profile" href="https://github.com/tjalp/">Take a look at tjalp's profile on GitHub</AnchorCard>
+        <AnchorCard title="tjalp's Profile" href="https://github.com/tjalp/">Take a look at tjalp''s profile on GitHub</AnchorCard>
         <AnchorCard title="Test Page" href="/test">Enter the temporary test page</AnchorCard>
         <AnchorCard title="404" href="/404">Navigate to a non-existent page</AnchorCard>
       </div>
