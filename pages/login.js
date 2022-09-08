c90c08bb41bbb0a5804b04baaec26996b4abbae6
tjalp <60233996+tjalp@users.noreply.github.com>
2022-09-08 22:39:11 +0200

    feat: add firebase & fix tabs

--- pages/login.js
+++ pages/login.js
@@ -3,14 +3,14 @@ import Layout from "../components/layouts/default";
 import GoogleLogin from "../components/google-login";
 
 export default function login() {
-    return (
-        <div>
-        <AnchorCard title="Go back" href="/">Go back to the home page</AnchorCard>
-        <GoogleLogin />
-        </div>
-    )
+  return (
+		<div>
+			<AnchorCard title="Go back" href="/">Go back to the home page</AnchorCard>
+			<GoogleLogin />
+		</div>
+  )
 }
 
 login.getLayout = function getLayout(page) {
-    return (<Layout>{page}</Layout>);
+	return (<Layout>{page}</Layout>);
 }
