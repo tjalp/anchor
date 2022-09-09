487ee42adb8e3404bee80933c31da4e5f2e2ad03
tjalp <60233996+tjalp@users.noreply.github.com>
2022-09-09 12:49:22 +0200

    feat: add 404 and test pages

---
+++ pages/test.js
@@ -0,0 +1,19 @@
+import AnchorCard from '../components/anchor-card'
+import Layout from '../components/layouts/default'
+
+export default function Test() {
+  return (
+    <div>
+      <div className="text-7xl text-center m-6 text-black dark:text-slate-400">
+        <span className="font-black bg-gradient-to-t from-[#4776E6] to-[#8E54E9] bg-clip-text text-transparent">Test Page</span>
+      </div>
+      <div className="grid grid-cols-1">
+        <AnchorCard title="Go Home" href="/">Return to the home page</AnchorCard>
+      </div>
+    </div>
+  )
+}
+
+Test.getLayout = function getLayout(page) {
+  return (<Layout>{page}</Layout>)
+}
