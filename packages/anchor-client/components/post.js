8f47a34229e9f9610b27c035773a7872e89c7c26
tjalp <60233996+tjalp@users.noreply.github.com>
2022-09-12 17:51:51 +0200

    improvement: proper formatting (to redeploy)

--- packages/anchor-client/components/post.js
+++ packages/anchor-client/components/post.js
@@ -2,32 +2,31 @@ import React, { useEffect, useState } from "react";
 import Axios from "axios";
 import AnchorCard from "./anchor-card";
 
-function Post({post_id}) {
+function Post({ post_id }) {
 
-    const [title, setTitle] = useState("Placeholder title");
-    const [content, setContent] = useState("Placeholder content");
+  const [title, setTitle] = useState("Placeholder title");
+  const [content, setContent] = useState("Placeholder content");
 
-    useEffect(() => {
-        Axios.get(`http://localhost:5000/api/v1/posts/id/${post_id}`).then((response)=>{
-            if (!response.data.error) 
-            {
-                setTitle(response.data.post.title);
-                setContent(response.data.post.content);
-            } else {
-                setTitle("Error loading post!");
-                setContent(response.error);
-            }
-        }).catch((err) => {
-            setTitle("An error occured while loading this post");
-            setContent(err.message);
-        });
-    }, [])
+  useEffect(() => {
+    Axios.get(`http://localhost:5000/api/v1/posts/id/${post_id}`).then((response) => {
+      if (!response.data.error) {
+        setTitle(response.data.post.title);
+        setContent(response.data.post.content);
+      } else {
+        setTitle("Error loading post!");
+        setContent(response.error);
+      }
+    }).catch((err) => {
+      setTitle("An error occured while loading this post");
+      setContent(err.message);
+    });
+  }, [])
 
-    return(
-        <div>
-            <AnchorCard title={title} href={"/posts"}>{content}</AnchorCard>
-        </div>
-    );
+  return (
+    <div>
+      <AnchorCard title={title} href={"/posts"}>{content}</AnchorCard>
+    </div>
+  );
 }
 
 export default Post;
