3f49ecdff60e5e761c0428b4099128ffe45c1db2
tjalp <60233996+tjalp@users.noreply.github.com>
2022-09-12 18:00:32 +0200

    feat: add web app capable

--- packages/anchor-client/pages/_document.js
+++ packages/anchor-client/pages/_document.js
@@ -4,7 +4,11 @@ import Script from 'next/script'
 export default function Document() {
   return (
     <Html className="bg-gray-100 dark:bg-zinc-800">
-      <Head />
+      <Head>
+        <meta name="apple-mobile-web-app-capable" content="yes" /> 
+        <meta name="mobile-web-app-capable" content="yes" /> 
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
       <body>
         <Main />
         <NextScript />
