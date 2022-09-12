f64e7d4f0269df17a394766ff7ba6735f69f6961
tjalp <60233996+tjalp@users.noreply.github.com>
2022-09-12 18:09:08 +0200

    improvement: indendation

--- packages/anchor-client/pages/posts.js
+++ packages/anchor-client/pages/posts.js
@@ -2,17 +2,13 @@ import AnchorCard from "../components/anchor-card";
 import Layout from "../components/layouts/default";
 import PostsList from "../components/postsList";
 
-
-
-export default function posts() {
-
-    return (<div>
-        <AnchorCard title="Go back" href="/">Go back to the home page</AnchorCard>
-        <PostsList />
-    </div>)
+export default function Posts() {
+  return (<div>
+    <AnchorCard title="Go back" href="/">Go back to the home page</AnchorCard>
+    <PostsList />
+  </div>)
 }
- 
 
-posts.getLayout = function getLayout(page) {
-    return (<Layout>{page}</Layout>)
+Posts.getLayout = function getLayout(page) {
+  return (<Layout>{page}</Layout>)
 }
