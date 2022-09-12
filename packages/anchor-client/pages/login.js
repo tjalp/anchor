4d7051475e4d03c6a1d07dfab6d91db03aa3ee7a
tjalp <60233996+tjalp@users.noreply.github.com>
2022-09-12 17:37:17 +0200

    feat: add title

--- packages/anchor-client/pages/login.js
+++ packages/anchor-client/pages/login.js
@@ -9,6 +9,9 @@ export default function login() {
 			<Head>
 				<title>Login</title>
 			</Head>
+			<div className="text-7xl text-center m-6 text-black dark:text-slate-400">
+        <span className="font-black bg-gradient-to-br from-[#4776E6] to-[#8E54E9] bg-clip-text text-transparent">Login</span>
+      </div>
 			<AnchorCard title="Go back" href="/">Go back to the home page</AnchorCard>
 			<GoogleLogin />
 		</>
--- packages/anchor-client/pages/posts.js
+++ packages/anchor-client/pages/posts.js
@@ -13,7 +13,7 @@ export default function posts() {
         <span className="font-black bg-gradient-to-br from-[#4776E6] to-[#8E54E9] bg-clip-text text-transparent">Posts</span>
       </div>
       <AnchorCard title="Go back" href="/">Go back to the home page</AnchorCard>
-      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 py-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 py-4">
         <Post post_id="631f138b745ca5b4764f6f64" />
         <Post post_id="631f138b745ca5b4764f6f65" />
       </div>
