432507aca35290ba8a3a3c23b2aeaf2bf2232173
tjalp <60233996+tjalp@users.noreply.github.com>
2022-09-12 15:02:25 +0200

    fix: indentation

--- packages/anchor-client/pages/posts.js
+++ packages/anchor-client/pages/posts.js
@@ -2,17 +2,18 @@ import AnchorCard from "../components/anchor-card";
 import Post from "../components/post";
 import Layout from "../components/layouts/default";
 
-
 export default function posts() {
-
-
-    return (<div>
-        <AnchorCard title="Go back" href="/">Go back to the home page</AnchorCard>
+  return (
+    <div>
+      <AnchorCard title="Go back" href="/">Go back to the home page</AnchorCard>
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 py-4">
         <Post post_id="631f138b745ca5b4764f6f64" />
-    </div>)
+      </div>
+    </div>
+  )
 }
 
 
 posts.getLayout = function getLayout(page) {
-    return (<Layout>{page}</Layout>)
+  return (<Layout>{page}</Layout>)
 }
