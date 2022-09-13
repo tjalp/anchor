49e3adcd52820c5aa4fa1595fa86971cc32e64e5
tjalp <60233996+tjalp@users.noreply.github.com>
2022-09-13 13:44:29 +0200

    fix: formatting

--- packages/anchor-client/pages/posts/[post].js
+++ packages/anchor-client/pages/posts/[post].js
@@ -4,40 +4,40 @@ import axios from "axios";
 import Layout from "../../components/layouts/default";
 import AnchorCard from "../../components/anchor-card";
 
-export default function post({ post_id }) {
+export default function Post({ post_id }) {
 
-    const [title, setTitle] = useState("Loading post...");
-    const [content, setContent] = useState("Loading content...");
-    const router = useRouter();
+  const [title, setTitle] = useState("Loading post...");
+  const [content, setContent] = useState("Loading content...");
+  const router = useRouter();
 
-    useEffect(() => {
-        if (router.isReady) 
-        {
-            axios.get(`${process.env.NEXT_PUBLIC_API_URL}/posts/id/${router.query.post}`).then((response) => {
-                if (!response.data.error) {
-                    setTitle(response.data.post.title);
-                    setContent(response.data.post.content);
-                } else {
-                    setTitle("Failed to find post!");
-                setContent("The post might not exist.");
-                }
-            }).catch((err) => {
-                console.error(`An error occurred: ${err}`)
-                setTitle("Failed to find post!");
-                setContent("The post might not exist.");
-            })
-        }   
-    }, [router.isReady])
+  useEffect(() => {
+    if (router.isReady) {
+      axios.get(`${process.env.NEXT_PUBLIC_API_URL}/posts/id/${router.query.post}`).then((response) => {
+        if (!response.data.error) {
+          setTitle(response.data.post.title);
+          setContent(response.data.post.content);
+        } else {
+          setTitle("Failed to find post!");
+          setContent("The post might not exist.");
+        }
+      }).catch((err) => {
+        console.error(`An error occurred: ${err}`)
+        setTitle("Failed to find post!");
+        setContent("The post might not exist.");
+      })
+    }
+  }, [router.isReady])
 
 
-    return (<div className="text-center dark:text-slate-100 text-slate-800">
-        <AnchorCard title={"Go back"} href={"/posts"}>Go back to posts</AnchorCard>
-        <h1 className="font-black text-7xl m-20 bg-gradient-to-br from-[#4776E6] to-[#8E54E9] bg-clip-text text-transparent">{title}</h1>
-        <div>{content}</div>
-        
-    </div>)
+  return (
+    <div className="text-center dark:text-slate-100 text-slate-800">
+      <AnchorCard title={"Go back"} href={"/posts"}>Go back to posts</AnchorCard>
+      <h1 className="font-black text-7xl m-20 bg-gradient-to-br from-[#4776E6] to-[#8E54E9] bg-clip-text text-transparent">{title}</h1>
+      <div>{content}</div>
+    </div>
+  )
 }
 
-post.getLayout = function getLayout(page) {
-    return (<Layout>{page}</Layout>)
+Post.getLayout = function getLayout(page) {
+  return (<Layout>{page}</Layout>)
 }
