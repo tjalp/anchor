ee891d6dd35576897690fa1fdc237abc6706cda2
tjalp <60233996+tjalp@users.noreply.github.com>
2022-09-15 20:05:14 +0200

    improvement: make the anchor card an actual card

--- packages/anchor-client/components/anchor-card.js
+++ packages/anchor-client/components/anchor-card.js
@@ -6,18 +6,17 @@ export default function AnchorCard({ title, href, children }) {
       <Link href={href}>
         <a>
         <div className="
-          transition ease-in-out duration-200
+          transition ease-in-out duration-300
           bg-white
           rounded-xl
           sm:rounded-lg
           shrink-0
           p-4
           text-left
-          shadow-xl
+          hover:shadow-lg
           hover:cursor-pointer
-          hover:bg-slate-100
+          hover:-translate-y-0.5
           dark:bg-zinc-700
-          dark:hover:bg-zinc-600
           dark:border-zinc-700
           dark:hover:border-zinc-600
         ">
