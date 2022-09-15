0809b2893611f9a5b792522df9f4365d9ce68eb5
tjalp <60233996+tjalp@users.noreply.github.com>
2022-09-15 20:12:00 +0200

    feat: follow nextjs' advise on the header

--- packages/anchor-client/components/layouts/default.js
+++ packages/anchor-client/components/layouts/default.js
@@ -1,6 +1,13 @@
+import Head from 'next/head'
+
 export default function Layout({ children }) {
   return (
     <>
+      <Head>
+        <meta name="apple-mobile-web-app-capable" content="yes" /> 
+        <meta name="mobile-web-app-capable" content="yes" /> 
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
       <main className="flex flex-col max-w-screen-2xl mx-auto p-4 font-sans">{children}</main>
     </>
   )
--- packages/anchor-client/pages/_document.js
+++ packages/anchor-client/pages/_document.js
@@ -4,11 +4,7 @@ import Script from 'next/script'
 export default function Document() {
   return (
     <Html className="bg-gray-100 dark:bg-zinc-800">
-      <Head>
-        <meta name="apple-mobile-web-app-capable" content="yes" /> 
-        <meta name="mobile-web-app-capable" content="yes" /> 
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
