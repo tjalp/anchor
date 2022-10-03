35905bac0e0bd0a69ab3c3c79919ae63b11055ef
tjalp <60233996+tjalp@users.noreply.github.com>
2022-10-03 08:51:22 +0200

    feat: basic challenges list page

--- packages/anchor-client/components/challenges-list.tsx
+++ packages/anchor-client/components/challenges-list.tsx
@@ -1,25 +1,30 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import AnchorCard from "./anchor-card";
-
+import LoadingIcon from "./loading-icon";
 
 export default function ChallengesList() {
 
-    const [challenges, setChallenges] = useState([]);
+  const [challenges, setChallenges] = useState([]);
 
-    useEffect(() => {
-        axios.get(`${process.env.NEXT_PUBLIC_API_URL}/challenges`).then((response) => {
-            if (!response.data.error) {
-                setChallenges(response.data.challenges);
-            } else {
-                console.error(response.data.error);
-            }
-        }).catch((e) => {
-            console.error(e);
-        })
-    }, [])
+  useEffect(() => {
+    axios.get(`${process.env.NEXT_PUBLIC_API_URL}/challenges`).then((response) => {
+      if (!response.data.error) {
+        setChallenges(response.data.challenges);
+      } else {
+        console.error(response.data.error);
+      }
+    }).catch((e) => {
+      console.error(e);
+    })
+  }, [])
 
-    return (<div>
+  return (
+    <>
+      {challenges.length === 0 && <div className="my-4 w-full flex items-center justify-center"><LoadingIcon /></div>}
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-0 my-4">
         {challenges.map(c => <AnchorCard title={c.title} href={`/challenges/${c._id}`}>{c.desc}</AnchorCard>)}
-    </div>)
+      </div>
+    </>
+  )
 }
--- packages/anchor-client/pages/challenges/index.tsx
+++ packages/anchor-client/pages/challenges/index.tsx
@@ -1,14 +1,26 @@
 import ChallengesList from "../../components/challenges-list";
 import LoginManager from "../../components/loginManager";
 import AnchorCard from "../../components/anchor-card";
+import { NextPageWithLayout } from "../_app";
+import { ReactElement } from "react";
+import Layout from "../../components/layouts/default";
 
-export default function challengesPage() {
-
-
-    return (<div>
-        <AnchorCard title="Go back" href="/">Go back to the home page</AnchorCard>
-        <LoginManager />
-        <ChallengesList />
-    </div>)
-
+const Challenges: NextPageWithLayout = () => {
+  return(
+    <>
+      <AnchorCard title="Go back" href="/">Go back to the home page</AnchorCard>
+      <LoginManager />
+      <ChallengesList />
+    </>
+  )
 }
+
+Challenges.getLayout = function getLayout(page: ReactElement) {
+  return (
+    <Layout>
+      {page}
+    </Layout>
+  )
+}
+
+export default Challenges
