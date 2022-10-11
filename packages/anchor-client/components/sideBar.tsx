d97945ff1d8a3c5ba230a3c0a40a79273d96da8e
hugo <hugo>
2022-10-11 11:35:25 +0000

    waarom werkt die sidebar nog niet? :(

--- packages/anchor-client/components/sideBar.tsx
+++ packages/anchor-client/components/sideBar.tsx
@@ -6,9 +6,9 @@ export default function SideBar(){
     const [dropMenuStyle] = useState("p-4 text-lg dark:text-white border border-offset-4 border-transparent rounded-lg hover:bg-slate-300 dark:hover:bg-neutral-700")
 
     return(
-        <div className="float-left h-screen mr-10 object-scale-down bg-slate-50 dark:bg-neutral-900 outline outline-1 outline-slate-300 dark:outline-neutral-600 outline-offset-2 select-none">
+        <div className="object-left-top max-w-fit h-screen mr-10 object-scale-down bg-slate-50 dark:bg-neutral-900 outline outline-1 outline-slate-300 dark:outline-neutral-600 outline-offset-2 select-none">
             <div>
-                <div className="m-5 text-3xl font-black bg-gradient-to-br from-[#ee0979] to-[#ff6a00] bg-clip-text text-transparent"><Link href="/home">⚓Anchor.</Link></div>
+                <div className="p-5 text-3xl font-black bg-gradient-to-br from-[#ee0979] to-[#ff6a00] bg-clip-text text-transparent"><Link href="/home">⚓Anchor.</Link></div>
                 <Link href="./"><div className={buttonStyle}>Home</div></Link>
                 <Link href="./posts"><div className={buttonStyle}>Posts</div></Link>
                 <Link href="./challenges"><div className={buttonStyle}>Challenges</div></Link>
@@ -18,8 +18,8 @@ export default function SideBar(){
                 <div className="flex flex-col-reverse">
                     <div className="peer p-4 text-xl dark:text-white border border-offset-5 border-transparent rounded-lg hover:bg-slate-200 dark:hover:bg-neutral-700">≡ Meer</div>
                     <div className="peer hidden peer-hover:block hover:block bg-slate-200 dark:bg-neutral-800">
-                        <div className={dropMenuStyle}><Link href="./settings">Instellingen</Link></div>
-                        <div className={dropMenuStyle}><Link href="./login?r=/home">In/Uit loggen</Link></div>
+                        <Link href="./settings"><div className={dropMenuStyle}>Instellingen</div></Link>
+                        <Link href="./login?r=/home"><div className={dropMenuStyle}>Uit loggen</div></Link>
                     </div>
                 </div>
             </div>       
--- packages/anchor-client/pages/Home.tsx
+++ packages/anchor-client/pages/Home.tsx
@@ -1,19 +1,35 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import Layout from '../components/layouts/default';
 import Head from "next/head";
-import AnchorCard from "../components/anchor-card";
 import SideBar from "../components/sideBar"
+import Post from "../components/postComponent.js"
+import { sortAndDeduplicateDiagnostics } from "typescript";
 
 export default function iets(){
-    
+  
+    const [posts, setPosts] = useState([])
+  
+    useEffect(()=>{
+      axios.get(`${process.env.NEXT_PUBLIC_API_URL}/posts?postPerPage=5`).then((response)=>{
+        if(!response.data.errer){
+          setPosts(response.data.posts)
+        }
+      }).catch((e)=>{
+        console.error(e)
+      })
+    })
+
     return(
         <>
         <Head>
           <title>⚓Anchor</title>
         </Head>
-        <SideBar></SideBar>
-        <p>ha</p>
+        <div>
+          <SideBar></SideBar>
+          <div className="mt-10">
+            {posts.map(p => <Post title={p.title} content={p.content} post_id={p._id} key={p._id} />)}
+          </div>
+        </div>
       </>
     )
 }
