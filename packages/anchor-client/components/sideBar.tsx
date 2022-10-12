5ad1950928214bed12f5d8c3b5dd10faa5f75b77
Hugo <hugoverhoeve@gmail.com>
2022-10-12 22:09:59 +0200

    dropup menu ;-;

--- packages/anchor-client/components/sideBar.tsx
+++ packages/anchor-client/components/sideBar.tsx
@@ -2,10 +2,11 @@ import Link from "next/link";
 import React, { useState } from "react";
 export default function SideBar(){
     
-    const [buttonStyle, setStyle] = useState("m-2 p-4 text-xl border border-offset-5 border-transparent rounded-lg hover:bg-slate-200")
-    
+    const [buttonStyle] = useState("m-2 p-4 text-xl dark:text-white border border-offset-5 border-transparent rounded-lg hover:bg-slate-200 dark:hover:bg-neutral-600")
+    const [dropMenuStyle] = useState("p-4 text-lg dark:text-white border border-offset-4 border-transparent rounded-lg hover:bg-slate-300 dark:hover:bg-neutral-700")
+
     return(
-        <div className="float-left mr-10 object-scale-down bg-slate-50 outline outline-1 outline-slate-300 outline-offset-2 grid grid-rows-2 divide-y select-none">
+        <div className="float-left h-screen mr-10 object-scale-down bg-slate-50 dark:bg-neutral-900 outline outline-1 outline-slate-300 dark:outline-neutral-600 outline-offset-2 select-none">
             <div>
                 <div className="m-5 text-3xl font-black bg-gradient-to-br from-[#ee0979] to-[#ff6a00] bg-clip-text text-transparent"><Link href="/home">⚓Anchor.</Link></div>
                 <Link href="./"><div className={buttonStyle}>Home</div></Link>
@@ -13,12 +14,15 @@ export default function SideBar(){
                 <Link href="./challenges"><div className={buttonStyle}>Challenges</div></Link>
                 <Link href="./tutorials"><div className={buttonStyle}>Tutorials</div></Link>
             </div>
-            <div className="mt-80">
-                <div className="m-2 p-4 border border-offset-5 border-transparent rounded-lg hover:bg-slate-200 group">
-                    <p id="p" className="hidden group-hover:block">hallo</p>
-                    Ξ Meer
+            <div className="absolute bottom-0 m-2 p-2">
+                <div className="flex flex-col-reverse">
+                    <div className="peer p-4 text-xl dark:text-white border border-offset-5 border-transparent rounded-lg hover:bg-slate-200 dark:hover:bg-neutral-700">≡ Meer</div>
+                    <div className="peer hidden peer-hover:block hover:block bg-slate-200 dark:bg-neutral-800">
+                        <div className={dropMenuStyle}><Link href="./settings">Instellingen</Link></div>
+                        <div className={dropMenuStyle}><Link href="./login?r=/home">In/Uit loggen</Link></div>
+                    </div>
                 </div>
-            </div>
+            </div>       
         </div>
     )
 }
