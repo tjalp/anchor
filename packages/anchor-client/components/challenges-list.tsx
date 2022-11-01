64567526b18f4ebbd2ed61750e87dae11fcd25dd
hugo <hugo>
2022-11-01 13:05:46 +0000

    voltooide challenge angeven

--- packages/anchor-client/components/challenges-list.tsx
+++ packages/anchor-client/components/challenges-list.tsx
@@ -3,13 +3,16 @@ import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import Link from "next/link"
 
-export default function ChallengesList({challenge_id, title, desc}) {
+export default function ChallengesList({challenge_id, title, desc, completed}) {
 
   return (
     <Link href={`/challenges/${challenge_id}`}>
       <div className="my-4 p-5 border border-slate-200 dark:border-neutral-600 bg-slate-50 dark:bg-zinc-700 rounded-lg hover:bg-slate-200 dark:hover:bg-zinc-600 cursor-pointer">
           <div className="text-2xl text-slate-900 dark:text-neutral-50">{title}</div>
-          <div className="text-lg text-slate-600 dark:text-neutral-400">{desc}</div>
+          <div className="text-lg text-slate-600 dark:text-neutral-400">
+            <div>{desc}</div>
+            <div>{`voltooid: ${completed}`}</div>
+          </div>
       </div>
     </Link>
   )
--- packages/anchor-client/pages/challenges/index.tsx
+++ packages/anchor-client/pages/challenges/index.tsx
@@ -1,7 +1,7 @@
 import ChallengesList from "../../components/challenges-list";
 import LoginManager from "../../components/loginManager";
-import { NextPageWithLayout } from "../_app";
 import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 import axios from "axios";
 import SideBar from "../../components/sideBar";
 import Head from "next/head";
@@ -9,17 +9,36 @@ import Head from "next/head";
 export default function challenges(){
 
   const [challenges, setChallenges] = useState([]);
+  const [userID, setUserID] = useState("");
+  const router = useRouter();
 
   useEffect(() => {
-      axios.get(`${process.env.NEXT_PUBLIC_API_URL}/challenges`).then((response) => {
+    if (router.isReady) {
+      if (localStorage.getItem("SignInToken") != null) {
+
+        axios.post(`${process.env.NEXT_PUBLIC_API_URL}/users/auth`, { token: localStorage.getItem("SignInToken") }).then((response) => {
           if (!response.data.error) {
-              setChallenges(response.data.challenges);
+            setUserID(response.data.userID);
           } else {
-              console.error(response.data.error);
+            console.error(response.data.error);
+            router.push(`/login?r=${router.pathname}`);
           }
-      }).catch((e) => {
+        }).catch((e) => {
           console.error(e);
-      })
+          router.push(`/login?r=${router.pathname}`);
+        });
+
+        axios.get(`${process.env.NEXT_PUBLIC_API_URL}/challenges`).then((response) => {
+            if (!response.data.error) {
+                setChallenges(response.data.challenges);
+            } else {
+                console.error(response.data.error);
+            }
+        }).catch((e) => {
+            console.error(e);
+        });
+      }
+    }
   }, [])
 
   return(
@@ -31,7 +50,7 @@ export default function challenges(){
     <div className="flex flex-row flex-nowrap h-screen select-none">
       <SideBar />
       <div className="float-right mt-14 max-w-16">
-        <div>{challenges.map(c => <ChallengesList title={c.title} desc={c.desc} challenge_id={c._id} key={c._id}/>)}</div>
+        <div>{challenges.map(c => <ChallengesList title={c.title} desc={c.desc} completed={c.completedUsers.includes(userID) ? "ja" : "nee"} challenge_id={c._id} key={c._id}/>)}</div>
       </div>
     </div>
   </>
